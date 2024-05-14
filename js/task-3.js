4a3235b329c330e2040b1a2791ba27cbc8f33d7b
Roman Choporov <ChoporovRoman@gmail.com>
2024-05-13 18:34:38 -0700

    Task 3

---
+++ js/task-3.js
@@ -0,0 +1,11 @@
+function getElementWidth(width, padding, border) {
+  width = parseFloat(width);
+  padding = parseFloat(padding);
+  border = parseFloat(border);
+
+  return width + padding * 2 + border * 2;
+}
+
+console.log(getElementWidth('50px', '8px', '4px'));     // 74
+console.log(getElementWidth('60px', '12px', '8.5px'));  // 101
+console.log(getElementWidth('200px', '0px', '0px'));    // 200
